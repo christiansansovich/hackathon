f81a7d5b667c06cbbc29d768542e7bdcca21ccf3
Christian Sansovich <csans@christiansmbp2.lan>
2022-03-17 00:23:01 -0400

    adding pre hopping into Ant's room

--- main.js
+++ main.js
@@ -1,95 +1,145 @@
-//https://github.com/cocosans/hackathon
-
-// https://github.com/coinapi/coinapi-sdk/tree/master/data-api/javascript-rest
-
 //API Keys
+import fetch from "node-fetch";
+globalThis.fetch = fetch
 const endpointURL = 'https://paper-api.alpaca.markets'
-const publicKey = 'PKWTIN11GNGTFGCT7XR4'
-const privateKey = 'mbOvSrkIDmIESSLKIfDh6xwiavc7Wtdh5z7pFeu1'
+const publicKey = 'PK6BDYN5QUXQIDY7022Y'
+const privateKey = 'vMvR4rjJ41sb2aLNKAXUTT46YCfA8lJMNST8FP2M'
 const headers = {'APCA-API-KEY-ID': publicKey, 'APCA-API-SECRET-KEY' : privateKey}
+const postHeaders = {'APCA-API-KEY-ID': publicKey, 'APCA-API-SECRET-KEY' : privateKey, 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
 
 
 //API endpoints
-const cryptoMarketData = '/v1beta1/crypto';
+const returnAccount = '/v2/account';
+const ordersInAccount = '/v2/orders';
+const positionsInAccount = '/v2/positions';
+const accountURL = endpointURL + returnAccount;
+const positionURL = endpointURL + positionsInAccount;
+const ordersURL = endpointURL + ordersInAccount;
+const streamURL = 'wss://stream.data.alpaca.markets/v1beta1/crypto';
 
-//https://www.youtube.com/watch?v=GsGeLHTOGAg
+//Driver Methods
+liveBitcoinPull();
+setInterval(updateData, 1000);
 
-//how to submit trades - https://www.youtube.com/watch?v=jzIJ-0rDvCo
-
-/**
- * @param {Number} coin - Number of coins to purchase
- * @param {Number} amount - Amount to use
- * @param {String} curr -  USD or coin of choice
- */
-
-function buy(coin, amount, curr) {
-  
+//Sets up stream and 
+function updateData() {
+  InfoPull(accountURL);
+  InfoPull(positionURL);
 }
-//taking some time to plan out the design. Lmk if you have some cool ideas
-// will do - did you see my slack? need terminal access to test console logs + quokka
-//responded in slack - gotchu
 
-let resultURL = endpointURL + cryptoMarketData;
+function buy(notional, symbol) {
+  // let qty = '1';
+  // let symbol = 'ETHUSD';
+  let type = 'market';
+  let side = 'buy';
+  let time_in_force = 'day';
 
-function get(coinType) {
-  const resultURL = endpointURL + cryptoMarketData;
-  const streamURL = 'wss://stream.data.alpaca.markets/v1beta1/crypto';
+  const bodyObject = {
+    notional : notional,
+    symbol : symbol,
+    type : type,
+    side : side,
+    time_in_force : time_in_force
+  }
 
+  const purchase = buyCoins(ordersURL, bodyObject);
+}
+
+async function buyCoins(url, data) {
+  const response = await fetch(url,{
+    method: 'POST',
+    headers : headers,
+    body : JSON.stringify(data)
+  });
+
+  return response.json();
+}
+
+function InfoPull(url) {
+  fetch(url, {
+    method : 'GET',
+    headers : headers
+  }).then((response) => {
+    return response.json();
+  }).then((response) => {
+    if (Array.isArray(response)) positionInfoPopulate(response); 
+    else accountInfoPopulate(response);
+  });
+}
+
+function positionInfoPopulate(accountInfo) {
+  const BTHholdings = document.querySelector('#btcholdings');
+  const ETHholdings = document.querySelector('#ethholdings');
+  
+  let BTHp = 0;
+  let ETHp = 0;
+
+  for (let pos of accountInfo) {
+    if (pos.symbol === 'BTCUSD') BTHp = pos.qty;
+    if (pos.symbol === 'ETHUSD') ETHp = pos.qty;
+  }
+  
+
+  BTHholdings.innerText = `Total Bitcoin Held: ${BTHp}`;
+  ETHholdings.innerText = `Total Etherium Held: ${ETHp}`;
+}
+
+function accountInfoPopulate(accountInfo) {
+  const buyingPower = document.querySelector('#buyingpower');
+  const cash = document.querySelector('#cash');
+  const equity = document.querySelector('#equity');
+  const currency = document.querySelector('#currency');
+
+  buyingPower.innerText = `Buying power: ${accountInfo.buying_power}`;
+  cash.innerText = `Cash: ${accountInfo.cash}`;
+  equity.innerText = `Equity: ${accountInfo.equity}`;
+  currency.innerText = `Currency: ${accountInfo.currency}`;
+}
+
+
+function liveBitcoinPull() {
   const listener = new WebSocket(streamURL);
-  console.log(listener.readyState);
-
-  listener.addEventListener('message', initialHandshake);
-
-  function initialHandshake(msg) {
-    outputMsg(msg);
-
-    this.send(`{"action":"auth","key":"${publicKey}","secret":"${privateKey}"}`)
-
-    this.addEventListener('open', outputMsg);
-  }
-
-  function outputMsg(msg) {
-    const output = msg.json();
-
-    for (let element of output) {
-      console.log(element.t)
-      console.log(element.msg);
+  
+  listener.addEventListener('message', (msg) => {
+    // console.log(msg.data);
+    if (msg.data === '[{"T":"success","msg":"connected"}]') {
+      listener.send(`{"action":"auth","key":"${publicKey}","secret":"${privateKey}"}`)
     }
-  }
-  
-  
-  
-  function dataImport() {
-    fetch(resultURL, {
-      'method' : 'GET',
-      'headers' : headers
-    }).then((data) => {
-      console.log(data);
+    if (msg.data === '[{"T":"success","msg":"authenticated"}]') {
+      listener.send('{"action":"subscribe","trades":["BTCUSD","ETHUSD"]}');
+      listener.addEventListener('message', updatePrice);
     }
-    );
+  });
+
+  function updatePrice(msg) {
+    const BTCPrice = document.querySelector('#BTC');
+    const ETHPrice = document.querySelector('#ETH');
+    const lastPrice = JSON.parse(msg.data);
+    
+    const [BTCp, ETHp] = getPrice(lastPrice);
+    
+    if (BTCp) BTCPrice.innerText = `BTC Price: ${BTCp}`;
+    if (ETHp) ETHPrice.innerText = `ETH Price: ${ETHp}`;
   }
   
+  function getPrice(arr) {
+    let eth = 0;
+    let btc = 0;
+
+    for (let msg of arr) {
+      if (msg.S === 'ETHUSD') eth = msg.p;
+      if (msg.S === 'BTCUSD') btc = msg.p;
+    }
+
+    return [btc, eth];
+  }
 }
 
-
 /**
- * buy
- *  -top 5 coins 
- *     -BTC Bitcoin
- *     -ETH Etherium
- *     -USDT Tether
- *     -BNB Binance Coin
- *     -USDC U.S. Dollar Coin 
- *  -either dollars or # of coins
+ * BTC price = BTC
+ * ETH price = ETH
+ * buyingpower = buyingpower
  * 
- * get price of coins
- *  -input type of coin
- *  -output coin price
- * 
- * 
- * 
- * stretch:
- * sell
- *  -either dollars or # of coins
- * 
+ * # of bitcoin = btcholdings
+ * # of etherium = ethholdings
  */
